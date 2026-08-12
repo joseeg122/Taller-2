2c4099acf418fdb69921941cd9c28ed89cdcde82
jose <josea.estrada@upb.edu.co>
2026-08-12 09:03:09 -0500

    me equivoque y borre el codigo del vareables y deje el de readme ahora se dejo el codigo en el vareables y borre el del readme

--- Variables.java
+++ Variables.java
@@ -1,3 +1,64 @@
 public class Variables {
-    
+    public static void main(String[] args) {
+
+        // Tipo byte (numeros enteros pequeños)
+        byte edad = 20;
+        byte cantidadHermanos = 2;
+
+        // Tipo short (numeros enteros medianos)
+        short anioNacimiento = 2005;
+        short poblacionCiudad = 12000;
+
+        // Tipo int (numeros enteros comunes)
+        int numeroEstudiantes = 350;
+        int codigoPostal = 50021;
+
+        // Tipo long (numeros enteros grandes)
+        long distanciaTierraLuna = 384400000L;
+        long numeroCuentaBancaria = 1234567890123L;
+
+        // Tipo float (numeros decimales, menos precision)
+        float precioLibro = 25.5f;
+        float estaturaMetros = 1.75f;
+
+        // Tipo double (numeros decimales, mas precision)
+        double precioCasa = 150000.75;
+        double pi = 3.14159265358979;
+
+        // Tipo char (un solo caracter)
+        char inicialNombre = 'J';
+        char calificacion = 'A';
+
+        // Tipo boolean (verdadero o falso)
+        boolean esEstudiante = true;
+        boolean tieneLicencia = false;
+
+        // Tipo String (cadenas de texto)
+        String nombre = "Juan Perez";
+        String ciudad = "Medellin";
+        String carrera = "Ingenieria de Sistemas";
+        String universidad = "Universidad Nacional";
+
+        // Imprimir todas las variables
+        System.out.println("Edad: " + edad);
+        System.out.println("Cantidad de hermanos: " + cantidadHermanos);
+        System.out.println("Anio de nacimiento: " + anioNacimiento);
+        System.out.println("Poblacion de la ciudad: " + poblacionCiudad);
+        System.out.println("Numero de estudiantes: " + numeroEstudiantes);
+        System.out.println("Codigo postal: " + codigoPostal);
+        System.out.println("Distancia Tierra-Luna (m): " + distanciaTierraLuna);
+        System.out.println("Numero de cuenta bancaria: " + numeroCuentaBancaria);
+        System.out.println("Precio del libro: " + precioLibro);
+        System.out.println("Estatura en metros: " + estaturaMetros);
+        System.out.println("Precio de la casa: " + precioCasa);
+        System.out.println("Valor de pi: " + pi);
+        System.out.println("Inicial del nombre: " + inicialNombre);
+        System.out.println("Calificacion: " + calificacion);
+        System.out.println("Es estudiante: " + esEstudiante);
+        System.out.println("Tiene licencia: " + tieneLicencia);
+        System.out.println("Nombre: " + nombre);
+        System.out.println("Ciudad: " + ciudad);
+        System.out.println("Carrera: " + carrera);
+        System.out.println("Universidad: " + universidad);
+    }
 }
